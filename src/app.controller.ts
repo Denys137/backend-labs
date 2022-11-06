2489d539990fb98de010f2653c83d1033621743a
StillUsefull <117542814+Denys137@users.noreply.github.com>
2022-11-06 15:18:41 +0300

    upgrade app controller with endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,66 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { User } from './user.entity';
+import { Category } from './category.entity';
+import { Cost } from './cost.entity';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Post('new-user')
+  createUser(@Body('user') user: User): any {
+    try {
+      this.appService.createUser(user);
+    } catch (e) {
+      return e.message;
+    }
+  };
+
+  @Post('new-category')
+  createCategory(@Body('new-category') newCategory: Category): any {
+    try {
+      this.appService.createCategory(newCategory);
+    } catch (e) {
+      return this.appService.newError(e);
+    }
+  }
+ 
+  @Post('new-cost')
+  
+  createCost(@Body('new-cost') newCost: Cost): any{
+    
+    try {
+      this.appService.createCost(newCost);
+    } catch (e) {
+      return this.appService.newError(e);
+    }
+  }
+  
+  @Get('categories')
+  
+  getCategories(): Category[] {
+    return this.appService.getCategories();
+  } 
+
+  
+  @Get(":id")
+  getCostsByUserId(@Param('id') id: number): any {
+    try {
+      return this.appService.getCostsByUserId(id);
+    } catch (e) {
+      return this.appService.newError(e);
+    }
+  }
+  
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getCostsById(@Query() query: any): any{
+    
+    try {
+      
+      return this.appService.getCostsById(query.userId, query.categoryId);
+    } catch (e) {
+      return this.appService.newError(e);
+    }
   }
 }
