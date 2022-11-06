1dfccafb700be3633fb2ac0ac3e6928f527a71c5
StillUsefull <117542814+Denys137@users.noreply.github.com>
2022-11-06 15:17:33 +0300

    upgrade app service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,67 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './user.entity';
+import { Category } from './category.entity';
+import { Cost } from './cost.entity';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  USERS: User[] = [];
+  CATEGORIES: Category[] = [];
+  COSTS: Cost[] = [];
+  createUser(newUser: User): any {
+    let user = new User(newUser.id, newUser.name);
+    this.USERS.push(user);
+    console.log(this.USERS);
+    
+  }
+
+  
+  createCategory(newCategory: Category): any {
+    let category = new Category(newCategory.id, newCategory.title)
+    this.CATEGORIES.push(category);
+    console.log(this.CATEGORIES); 
+  }
+
+  
+  createCost(newCost: Cost): any {
+    
+      let cost = new Cost(newCost.id, newCost.userId, newCost.categoryId, newCost.sum);
+      this.COSTS.push(cost);
+      console.log(this.COSTS);
+    
+  }
+
+
+  getCategories(): Category[] {
+    return this.CATEGORIES;
+  }
+
+  getCostsByUserId(userId: any): Cost[] {
+    let array: Cost[] = [];
+    this.COSTS.forEach((item): void => {
+      if (item.userId == +userId) {
+        array.push(item);
+      }
+    })
+
+    return array;
+  }
+
+  getCostsById(userId: number, categoryId: number): any {
+    let array: any = [];
+    
+    this.COSTS.forEach((item): void => {
+      if (item.userId == +userId && item.categoryId == +categoryId){
+        array.push(item);
+      }
+    })
+    return array;
+    
+  }
+  
+  newError(e: Error): string {
+    console.log(e.message);
+    return "Something is wrong with data"
   }
 }
