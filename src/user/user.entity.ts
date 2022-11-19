897cb47f3d9eb089292063dca00de3437be297c2
StillUsefull <117542814+Denys137@users.noreply.github.com>
2022-11-19 23:28:06 +0200

    add currency (lab2)

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,10 +1,12 @@
-
+ import { Currency } from "src/currency/currency.enum";
 export class User {
     id: number;
     name: string;
-    constructor(id, name){
+    currency: Currency
+    constructor(id, name, currency: Currency = Currency.UAH){
         this.id = id;
         this.name = name;
+        this.currency = currency;
     }
 
 }
